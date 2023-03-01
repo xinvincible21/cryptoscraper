8b3f7ee508de5052649dd3b74af5bd466da8b3d2
xinvincible21 <xinvincible21@gmail.com>
2023-03-01 13:45:36 -0500

    fix killing of app

--- src/main/resources/run_server.sh
+++ src/main/resources/run_server.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 start() {
-    java -cp ~/cryptoscraper/target/scala-2.12/theorg-assembly-0.1.0-SNAPSHOT.jar com.invincible.CryptoApp > /tmp/server.log &
+    java -cp ~/cryptoscraper/target/scala-2.12/cryptoscraper-assembly-0.1.0-SNAPSHOT.jar com.invincible.CryptoApp > /tmp/server.log &
+    PID=$!
+    echo "starting $PID"
 }
 
 stop() {
-   kill $(ps aux | grep 'com.invincible.CryptoApp' | awk '{print $2}')
+   PID=`ps -eaf | grep 'com.invincible.CryptoApp' | grep -v grep | awk '{print $2}'`
+   if [[ "" !=  "$PID" ]]; then
+	   echo "killing $PID"
+	   kill -9 $PID
+   fi
 }
 
 case "$1" in
@@ -20,7 +26,7 @@ case "$1" in
        start
        ;;
     status)
-       ps aux | grep 'com.invincible.CryptoApp'
+       ps -eaf | grep 'com.invincible.CryptoApp' | grep -v grep
        ;;
     *)
        echo "Usage: $0 {start|stop|status|restart}"
